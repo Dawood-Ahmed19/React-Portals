36e10d7f7b6af65ff420c8c3286bfe4dfb89b043
Dawood-Ahmed19 <gfxwork66@gmail.com>
2023-07-05 20:35:51 +0500

    feat: added Profiler

--- src/modal.jsx
+++ src/modal.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { Profiler } from "react";
 import { ReactDOM } from "react";
 import { createPortal } from "react-dom";
 
 const Modal = ({ show, closeModal }) => {
   if (!show) return null;
+  function callback(
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime,
+    interactions
+  ) {
+    console.log(
+      id,
+      phase,
+      actualDuration,
+      baseDuration,
+      startTime,
+      commitTime,
+      interactions
+    );
+  }
   return createPortal(
     <>
-      <div className="modal">
-        <div className="overlay"></div>
-        <div className="content">
-          <input type="text" name="Name" placeholder="Type Your Name here" />
-          <input
-            type="password"
-            name="Password"
-            placeholder="type Your Password here"
-          />
-          <button type="submit">Submit</button>
-          <button onClick={closeModal}>Close</button>
+      <Profiler id="Modal" onRender={callback}>
+        <div className="modal">
+          <div className="overlay"></div>
+          <div className="content">
+            <input type="text" name="Name" placeholder="Type Your Name here" />
+            <input
+              type="password"
+              name="Password"
+              placeholder="type Your Password here"
+            />
+            <button type="submit">Submit</button>
+            <button onClick={closeModal}>Close</button>
+          </div>
         </div>
-      </div>
+      </Profiler>
     </>,
     document.getElementById("modal-root")
   );
